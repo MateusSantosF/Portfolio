483f919b74ddf7191101a1710e5e796cdc46d5ae
MateusSantosF <mateusferreira.dev@gmail.com>
2024-06-09 16:24:21 -0300

    style: :lipstick: adjust home page details

--- src/pages/home/components/LandPage/index.tsx
+++ src/pages/home/components/LandPage/index.tsx
@@ -1,11 +1,12 @@
 import Badge from "@shared/components/Badge";
 import Button from "@shared/components/Button";
+import HashLink from "@shared/components/HashLink";
 import Link from "@shared/components/Link";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
 
 function LandPage() {
   return (
-    <section className="h-lvh px-6">
+    <section className="h-[75vh] px-6" id="home">
       <section className="flex flex-1 flex-wrap-reverse lg:flex-nowrap lg:items-center gap-6 h-full">
         <div className="flex w-full lg:items-start items-center flex-col gap-6">
           <Badge color="stone" value="Mateus Ferreira" />
@@ -14,10 +15,10 @@ function LandPage() {
           </h1>
 
           <p className="text-md text-gray-500 -mt-3">
-            Python / React / Next.js / NodeJS / Azure Devops
+            Python | CSharp |  Next.js | NodeJS |  Azure Devops
           </p>
           <div className="flex gap-3">
-            <Button>Meus projetos</Button>
+            <HashLink href="#projects"><Button>Meus projetos</Button></HashLink>
             <Button color="secondary">Baixar CV</Button>
           </div>
           <div className="flex w-full md:w-min items-center justify-center gap-3">
@@ -37,7 +38,10 @@ function LandPage() {
           </div>
         </div>
         <div className="flex w-full items-center justify-center lg:mb-24">
-          <img className="object-fit rounded-full max-w-[35%] lg:max-w-[70%]" src="https://avatars.githubusercontent.com/u/62969620?v=4"/>
+          <img
+            className="object-fit rounded-full max-w-[35%] lg:max-w-[70%]"
+            src="https://avatars.githubusercontent.com/u/62969620?v=4"
+          />
         </div>
       </section>
     </section>
